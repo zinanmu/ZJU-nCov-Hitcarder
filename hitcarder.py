1d609fc2401e5b6c364e876fdd68230a7a14da11
zinanmu <98010977+zinanmu@users.noreply.github.com>
2022-10-19 15:57:06 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -88,12 +88,12 @@ class HitCarder(object):
             new_form = re.findall(r'<ul>[\s\S]*?</ul>', html)[0]
         except IndexError as _:
             raise RegexMatchError('Relative info not found in html with regex')
-
+            
+        with open("form.txt", "w", encoding="utf-8") as f:
+             f.write(new_form)
         with open("form.txt", "r", encoding="utf-8") as f:
             if new_form == f.read():
                 return True
-        #with open("form.txt", "w", encoding="utf-8") as f:
-        #     f.write(new_form)
         return False
 
     def get_info(self, html=None):
